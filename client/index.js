607fc6f5c33a5c5d1f68455838a8e45dc950150f
joeyklee <jsphknglee@gmail.com>
2018-12-27 21:09:29 -0800

    added links, linkrefs and projects services, added links dependencies

--- client/index.js
+++ client/index.js
@@ -13,6 +13,7 @@ if (process.env.NODE_ENV !== 'production') {
 app.use(require('./stores/clicks'))
 
 app.route('/', require('./views/main'))
+app.route('/dev-ref', require('./views/dev-ref'))
 app.route('/*', require('./views/404'))
 
 module.exports = app.mount('body')
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,13 @@
 const users = require('./users/users.service.js');
 const playlists = require('./playlists/playlists.service.js');
+const links = require('./links/links.service.js');
+const projects = require('./projects/projects.service.js');
+const linkrefs = require('./linkrefs/linkrefs.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(playlists);
+  app.configure(links);
+  app.configure(projects);
+  app.configure(linkrefs);
 };
