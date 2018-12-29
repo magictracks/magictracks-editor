25b2bc003b12d48b6f65c8dffb8e90e395ce09ac
joeyklee <jsphknglee@gmail.com>
2018-12-28 17:47:45 -0800

    rm refs services - TESTING

--- src/services/index.js
+++ src/services/index.js
@@ -2,16 +2,10 @@ const users = require('./users/users.service.js');
 const playlists = require('./playlists/playlists.service.js');
 const links = require('./links/links.service.js');
 const projects = require('./projects/projects.service.js');
-const linkrefs = require('./linkrefs/linkrefs.service.js');
-const playlistrefs = require('./playlistrefs/playlistrefs.service.js');
-const projectrefs = require('./projectrefs/projectrefs.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(playlists);
   app.configure(links);
   app.configure(projects);
-  app.configure(linkrefs);
-  app.configure(playlistrefs);
-  app.configure(projectrefs);
 };
